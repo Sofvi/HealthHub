622cb5ef63db94730c48416636c8c7ec6bded3eb
Tuomo <tuomo.muttonen@hotmail.com>
2021-12-06 20:09:23 +0200

    Lisätty ruoka ja paino activityille toiminnallisuuksia.

--- app/src/main/java/com/example/terveyssovellus/Weight.java
+++ app/src/main/java/com/example/terveyssovellus/Weight.java
@@ -3,36 +3,65 @@ package com.example.terveyssovellus;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
+import android.widget.ListView;
 
+import java.text.DateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Collections;
+import java.util.Date;
 import java.util.List;
 
 public class Weight extends AppCompatActivity {
 
-    private List<Integer> weight = new ArrayList<Integer>();
-    private ArrayAdapter<Weight> listAdapter;
+    Button bt;
+    EditText et;
+    ListView lv;
+    ArrayList<String> weight;
+    ArrayAdapter<String> arrayadapter;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_weight);
+
+        et = findViewById(R.id.editTxWeight);
+        bt = findViewById(R.id.button);
+        lv = findViewById(R.id.weightListview);
+
+        weight = new ArrayList<String>();
+        arrayadapter = new ArrayAdapter<String>(
+                Weight.this,
+                android.R.layout.simple_list_item_1,
+                weight);
+        lv.setAdapter(arrayadapter);
+
+        onClick();
+
     }
 
-    public void updateWeight() {
-        EditText editText = findViewById(R.id.editTxWeight);
-        int n = Integer.parseInt(editText.getText().toString());
-        weight.add(n);
-        updateListview();
 
+
+    public void onClick() {
+        bt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Calendar calendar = Calendar.getInstance();
+                String currentDate = DateFormat.getDateInstance().format(calendar.getTime());
+                String s = currentDate + ", Paino: " + et.getText().toString() + " kg";
+                weight.add(s);
+                Collections.reverse(weight);
+                arrayadapter.notifyDataSetChanged();
+            }
+        });
     }
-
-    private void updateListview() {
-    }
-
 
 
 }
+
