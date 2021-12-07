42dd45477aff70b3748ad3d241c71b098b09dabe
Tuomo <tuomo.muttonen@hotmail.com>
2021-12-07 15:34:23 +0200

    usersettings activity 2

--- app/src/main/java/com/example/terveyssovellus/UserSettings.java
+++ app/src/main/java/com/example/terveyssovellus/UserSettings.java
@@ -1,8 +1,12 @@
 package com.example.terveyssovellus;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class UserSettings extends AppCompatActivity {
 
@@ -10,5 +14,31 @@ public class UserSettings extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_settings);
+
+        EditText addCourseText = findViewById(R.id.et_food_goal);
+        addCourseText.setOnKeyListener(new View.OnKeyListener()
+        {
+            public boolean onKey(View v, int keyCode, KeyEvent event)
+            {
+                if (event.getAction() == KeyEvent.ACTION_DOWN)
+                {
+                    switch (keyCode)
+                    {
+                        case KeyEvent.KEYCODE_DPAD_CENTER:
+                        case KeyEvent.KEYCODE_ENTER:
+                            changeUserFood();
+                            return true;
+                        default:
+                            break;
+                    }
+                }
+                return false;
+            }
+        });
+    }
+
+    public void changeUserFood(){
+        Toast.makeText(getApplicationContext(),"Päivittäinen kaloritavoite asetettu.", Toast.LENGTH_SHORT).show();
+
     }
 }
