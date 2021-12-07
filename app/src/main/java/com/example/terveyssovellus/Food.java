d039144dd36a4e1425c219af2b659d259f915387
Tuomo <tuomo.muttonen@hotmail.com>
2021-12-07 13:30:15 +0200

    Ruoka kalorit tallentuu :)

--- app/src/main/java/com/example/terveyssovellus/Food.java
+++ app/src/main/java/com/example/terveyssovellus/Food.java
@@ -1,11 +1,15 @@
 package com.example.terveyssovellus;
 
+import static android.preference.PreferenceManager.getDefaultSharedPreferences;
+
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class Food extends AppCompatActivity {
 
@@ -15,6 +19,12 @@ public class Food extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_food);
+
+        SharedPreferences prefs = getDefaultSharedPreferences(getApplicationContext());
+        foodCalories = prefs.getInt("ruokakalorit",
+                0);
+        TextView textView = findViewById(R.id.total_food_calories);
+        textView.setText(String.valueOf(foodCalories));
     }
 
     public void updateFoodCalories(View view){
@@ -23,5 +33,14 @@ public class Food extends AppCompatActivity {
         foodCalories = foodCalories+n;
         TextView textView = findViewById(R.id.total_food_calories);
         textView.setText(String.valueOf(foodCalories));
+        Toast.makeText(getApplicationContext(),n + " Kaloria lisätty.", Toast.LENGTH_SHORT).show();
+        editText.getText().clear();
+
+        SharedPreferences prefs = getDefaultSharedPreferences(getApplicationContext());
+        SharedPreferences.Editor editor = prefs.edit();
+        editor.putInt("ruokakalorit", foodCalories);
+        editor.commit();
     }
+
+
 }
