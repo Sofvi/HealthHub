d9f5f1ac3dc8cbd552160e8eef38b02e43550bcd
Moikonna <moikamut@gmail.com>
2021-12-08 12:55:14 +0200

    Button to Exercise layout and Exercise.java changes

--- app/src/main/java/com/example/terveyssovellus/Exercise.java
+++ app/src/main/java/com/example/terveyssovellus/Exercise.java
@@ -1,14 +1,26 @@
 package com.example.terveyssovellus;
 
+import static android.preference.PreferenceManager.getDefaultSharedPreferences;
+
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.Spinner;
 
+import com.google.gson.Gson;
+
 import java.lang.reflect.Array;
+import java.text.DateFormat;
+import java.util.Calendar;
+import java.util.Collections;
 
 public class Exercise extends AppCompatActivity {
+    Button esub;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,4 +37,31 @@ public class Exercise extends AppCompatActivity {
         arrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         liikunnatDropdown.setAdapter(arrayAdapter);
     }
+   /* public void onClick() {
+        esub.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Calendar calendar = Calendar.getInstance();
+                String currentDate = DateFormat.getDateInstance().format(calendar.getTime());
+                String s = currentDate + " Paino: " + et.getText().toString() + " kg";
+                if(s.contains(",")){
+                    s = s.replace(",",".");
+                    Log.d("yeet2", s);
+                }
+                weight.add(s);
+                Collections.reverse(weight);
+                arrayadapter.notifyDataSetChanged();
+                SharedPreferences prefs = getDefaultSharedPreferences(getApplicationContext());
+                SharedPreferences.Editor editor = prefs.edit();
+                Gson gson = new Gson();
+                String json = gson.toJson(weight);
+                editor.putString("paino", json);
+                editor.apply();
+            }
+
+
+        });
+    }
+    */
+    
 }
