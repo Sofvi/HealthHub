0345bbfcae93c40e438a1226ee18e1c1617f3a5a
Tuomo <tuomo.muttonen@hotmail.com>
2021-12-08 15:28:21 +0200

    bmi laskuri

--- app/src/main/java/com/example/terveyssovellus/Statistics.java
+++ app/src/main/java/com/example/terveyssovellus/Statistics.java
@@ -1,14 +1,51 @@
 package com.example.terveyssovellus;
 
+import static android.preference.PreferenceManager.getDefaultSharedPreferences;
+
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
 
 public class Statistics extends AppCompatActivity {
+
+    private double weight = 0;
+    private int height = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_statistics);
+
+        SharedPreferences prefs = getDefaultSharedPreferences(getApplicationContext());
+        String paino = prefs.getString("paino",
+                "0");
+        String s1 = paino.substring(paino.indexOf(":")+1); //thx to ItamarG3 from stackoverflow
+        paino = s1.replace(" kg","");
+        s1 = paino.replace("\"]","");
+        paino = s1.split("\"")[0]; // thx to assylias from stackoverflow
+
+        weight = Double.parseDouble(paino);
+
+        SharedPreferences prefs2 = getDefaultSharedPreferences(getApplicationContext());
+        height = prefs2.getInt("user_height",
+                0);
+
+        double bmi = weight / ((double)height/100) / ((double) height/100);
+        String bmi2 = String.format("%.2f", bmi);
+
+        TextView textView = findViewById(R.id.tv_bmi);
+        textView.setText("Painoindeksi: " + bmi2);
+
+
+
+
+
+
+
     }
+
+
 }
--- app/src/main/java/com/example/terveyssovellus/UserSettings.java
+++ app/src/main/java/com/example/terveyssovellus/UserSettings.java
@@ -20,8 +20,8 @@ public class UserSettings extends AppCompatActivity {
         setContentView(R.layout.activity_user_settings);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
-        EditText addCourseText = findViewById(R.id.et_food_goal);
-        addCourseText.setOnKeyListener(new View.OnKeyListener()
+        EditText editFood = findViewById(R.id.et_food_goal);
+        editFood.setOnKeyListener(new View.OnKeyListener()
         {
             public boolean onKey(View v, int keyCode, KeyEvent event)
             {
@@ -41,11 +41,38 @@ public class UserSettings extends AppCompatActivity {
             }
         });
 
+        EditText editHeight = findViewById(R.id.et_height);
+        editHeight.setOnKeyListener(new View.OnKeyListener()
+        {
+            public boolean onKey(View v, int keyCode, KeyEvent event)
+            {
+                if (event.getAction() == KeyEvent.ACTION_DOWN)
+                {
+                    switch (keyCode)
+                    {
+                        case KeyEvent.KEYCODE_DPAD_CENTER:
+                        case KeyEvent.KEYCODE_ENTER:
+                            changeUserHeight();
+                            return true;
+                        default:
+                            break;
+                    }
+                }
+                return false;
+            }
+        });
+
         SharedPreferences prefs = getDefaultSharedPreferences(getApplicationContext());
         int calories_goal = prefs.getInt("user_food_goal",
                 0);
         TextView textView = findViewById(R.id.et_food_goal);
         textView.setText(String.valueOf(calories_goal));
+
+        SharedPreferences prefs2 = getDefaultSharedPreferences(getApplicationContext());
+        int user_height = prefs2.getInt("user_height",
+                0);
+        TextView textView2 = findViewById(R.id.et_height);
+        textView2.setText(String.valueOf(user_height));
     }
 
     public void changeUserFood(){
@@ -58,6 +85,17 @@ public class UserSettings extends AppCompatActivity {
         SharedPreferences.Editor editor = prefs.edit();
         editor.putInt("user_food_goal", n);
         editor.commit();
+    }
 
+    public void changeUserHeight(){
+        Toast.makeText(getApplicationContext(),"Pituus asetettu.", Toast.LENGTH_SHORT).show();
+
+        EditText editText = findViewById(R.id.et_height);
+        int n = Integer.parseInt(editText.getText().toString());
+
+        SharedPreferences prefs = getDefaultSharedPreferences(getApplicationContext());
+        SharedPreferences.Editor editor = prefs.edit();
+        editor.putInt("user_height", n);
+        editor.commit();
     }
 }
