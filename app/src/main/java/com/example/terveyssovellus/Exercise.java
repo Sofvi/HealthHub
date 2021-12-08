4e564fa420b2dda8dad3ce75e1f8e2ffc5ed7601
Moikonna <moikamut@gmail.com>
2021-12-08 14:29:11 +0200

    Ui changes to activity_exercise.xml and related java code

--- app/src/main/java/com/example/terveyssovellus/Exercise.java
+++ app/src/main/java/com/example/terveyssovellus/Exercise.java
@@ -11,6 +11,7 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
 import android.widget.Spinner;
 import android.widget.Toast;
 
@@ -18,18 +19,31 @@ import com.google.gson.Gson;
 
 import java.lang.reflect.Array;
 import java.text.DateFormat;
+import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Collections;
 
 public class Exercise extends AppCompatActivity {
     Button nappi;
     String sel;
+    ListView elv;
+    ArrayList<String> exer;
+    ArrayAdapter<String> arrayadapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_exercise);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         nappi = findViewById(R.id.esub);
+        elv = findViewById(R.id.exerciseList);
+        exer = new ArrayList<String>();
+
+        arrayadapter = new ArrayAdapter<String>(
+                Exercise.this,
+                android.R.layout.simple_list_item_1,
+                exer);
+        elv.setAdapter(arrayadapter);
+
         Spinner liikunnatDropdown = (Spinner) findViewById(R.id.spinnerExercises);
 
         ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(
@@ -60,7 +74,8 @@ public class Exercise extends AppCompatActivity {
                 Calendar calendar = Calendar.getInstance();
                 String currentDate = DateFormat.getDateInstance().format(calendar.getTime());
                 String s = currentDate + " liikuntamuoto " + sel;
-
+                Log.d("yeet", s);
+                exer.add(s);
                 SharedPreferences prefs = getDefaultSharedPreferences(getApplicationContext());
                 SharedPreferences.Editor editor = prefs.edit();
                 Gson gson = new Gson();
